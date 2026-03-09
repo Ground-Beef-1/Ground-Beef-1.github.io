cb19cc3da7dc4a335322913201a93b3ef50107a0
Ground-Beef-1 <mjbiluvdogs@gmail.com>
2026-03-09 17:04:41 -0400

    More game, more ending (no good ones)

--- projects/game/game.js
+++ projects/game/game.js
@@ -58,6 +58,58 @@ function StoneBend() {
 	}
 	waitForInput(processInput);
 }
+
+function Creature() {
+	clear();
+	print("\nYou continue down the path and see something you have never seen before. At first through the fog you see a lean, animalistic beast-like shape that looks like it is made of what you deeply desire and what you most fear." + "\nAs you approach closer and the creature itself becomes visible it reveals that it is much more physical, much more real if that makes any sense. It looks like a grotesque twisting of deer and dog with extrusions only describable as broken bones fashioned into sharp sabres, it looks painful but you see no trace of such feeling in its bright, hollow eyes. You see nothing but a reflection of your own soul but somehow you can tell it still can’t see you, but it likely will soon. But as scary this thing may seem, you catch a glimpse of a possible way out, this could be your only shot to escape.");
+	print("\n\t1-    Maybe it is a nice dog-beast-deer-monstrosity creature, you could be the next disney princess with your eldritch looking companion! You want to approach it and let it sniff your hand." + "\n\n\t2-    NOPE NOPE ALL THE WAY NOPE! Get me out of here, we are turning around and finding another way" + "\n\n\t3-    Cover yourself in leaves and twigs to sneak past it, if you move slowly and methodically it might not see you." + "\n\n\t4-    If you run fast enough you might just be able to get by, It’s better than nothing at least, besides, you were the fastest runner in second grade, you probably still have it in you!" + "\n\n\t5-    Use an item you have collected");
+	function processInput(input){
+		if (input.toLowerCase() === "1"){
+			MisplacedTrust();
+		}
+		else if (input.toLowerCase() === "2"){
+			StoneBend();
+		}
+		else if (input.toLowerCase() === "3"){
+			NoStealth();
+		}
+		else if (input.toLowerCase() === "4"){
+			NotSecondGrade();
+		}
+		else if (input.toLowerCase() === "5"){
+			ItemIdea();
+		}
+	}
+	waitForInput(processInput);
+}
+function NoStealth
+
+function MisplacedTrust() {
+	clear();
+	print("\nYou approach slowly and respectfully, it leans down and sniffs your hand while you think you may have just made a new companion! You already have plans of keeping it and living with it forever! You would finally have someone to talk to, someone to trust, someone to live for and something to die for.");
+	print("\nAs you are in your wonderland the creature immediately bites off your hand, once you snap to reality you don’t even have time to think before it opens its toothy maw and it masticates your flesh from bone and swallows you to fuel itself for the next fool that dares to approach it.");
+	print("\n\tPress C to continue");
+
+	function processInput(input){
+		if (input.toLowerCase() === "c"){
+			MisplacedTrust2();
+		}
+	}
+	waitForInput(processInput);
+}
+function MisplacedTrust2() {
+	clear();
+	print("\nBAD ENDING: MISPLACED TRUST" + "\nThose movies are fiction for a reason… This is not how abominations work, maybe dogs or cats- but definitely not that thing. Rethink your reality, this is not a kids show where everyone gets along. Did you not realize that once you woke up here? Or even after you saw that monstrosity? If the world was kind, something of that sort would not have existed in the first place");
+	print("\n\tPress R to restart");
+
+	function processInput(input){
+		if (input.toLowerCase() === "r"){
+			Clearing();
+		}
+	}
+	waitForInput(processInput);
+}
+
 function BendInspect() {
 	clear();
 	print("\nYou find a thick jacket made of a strong material, feeling a chill in the air you feel inclined to put it on, it smells like old cologne and leaf decay, which it matches the color of.");
@@ -97,7 +149,15 @@ function Clearing() {
 
 function Unwritten(){
 	clear();
-	print("I have not gotten so far as to add this arc yet, I'm just a guy");
+	print("\nI have not gotten so far as to add this arc yet, I'm just a guy");
+	print("\n\tPress R to restart");
+
+	function processInput(input){
+		if (input.toLowarCase() === "r"){
+			Clearing();
+		}
+	}
+	waitForInput(processInput);
 
 }
 
@@ -110,8 +170,8 @@ function Quitter() {
     print("\n\tPress C to continue");
     
     function processInput(input){
-        if (input.toLowerCase() === "1") {
-            Clearing();
+        if (input.toLowerCase() === "c") {
+            Quitter2();
         } else {
             stayHere();
             waitThenCall(Quitter);
@@ -120,11 +180,27 @@ function Quitter() {
     waitForInput(processInput);
 }
 
+function Quitter2() {
+	clear();
+	print("\nBAD ENDING: QUITTER" + "\nThe death of your hope leads to your own demise. You could have tried, you know that… Right?");
+	print("\n\tPress R to restart");
+
+	function processInput(input){
+		if (input.toLowerCase() === "r"){
+			Clearing();
+		}
+	}
+	waitForInput(processInput);
+}
+
+
+
+
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
 function start(){
-    print("Welcome, press any key to start");
+    print("Open your eyes. Enter to open your eyes.");
 
     function processInput(input){
             Clearing();
