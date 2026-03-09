7d3c5c53d242698b332ff04b288513bfa54112c5
Ground-Beef-1 <mjbiluvdogs@gmail.com>
2026-03-09 16:20:10 -0400

    You can get an item kinda (I have yet to make it a variable)

--- projects/game/game.js
+++ projects/game/game.js
@@ -9,10 +9,12 @@ let gameActive = true; //this variable is required.
 
 //Make one function for each location
 
+
+
 function StonePath() {
 	clear();
 	print("\nYou inspect the worn stone path, who placed these stones? Were there others in your own situation, or perhaps something else…?, you feel uneasy but that is probably from the absurdity of your situation, but what else is new here");
-	print("\n\t1-    Turn around and do something else, the vibes are off"   +  "\n\t2-   Continue down the path, this could be the way out!" + "\n\t3-   Inspect further, maybe there’s something under the stones?");
+	print("\n\t1-   Turn around and do something else, the vibes are off"   +  "\n\t2-   Continue down the path, this could be the way out!" + "\n\t3-   Inspect further, maybe there’s something under the stones?");
 	function processInput(input){
 		if (input.toLowerCase() === "1"){
 			Clearing();
@@ -22,6 +24,49 @@ function StonePath() {
 		}
 		else if (input.toLowerCase() === "3"){
 			StoneInspect();
+		}
+	}
+	waitForInput(processInput);
+}
+function StoneInspect() {
+	clear();
+	print("\nThere was nothing under the stones but worms and beetles. What did you even expect? You wonder, and now you look a fool, you feel mocked by the roots of the trees and judged from the stars in the sky. Clearly your subconscious has some sort of social anxiety.");
+	print("\n\tPress B to go back");
+	function processInput(input){
+		if (input.toLowerCase() === "b"){
+			StonePath();
+		}
+	}
+	waitForInput(processInput);
+}
+
+
+function StoneBend() {
+	clear();
+	print("\nYou continue on the path and reach a bend, beyond this you see nothing but more stone path and a guttural feeling to turn around and leave passes through you. You do not feel safe, but was any of this ever safe?");
+	print("\n\t1-    Inspect the place? If nothing else it might postpone what could happen if you continue" + "\n\t2-    Keep going, this could still be the way out" + "\n\t3-    Trust your gut and turn around, this path likely leads somewhere you don’t want to go");
+	function processInput(input){
+		if (input.toLowerCase() === "1"){
+			BendInspect();
+		}
+		else if (input.toLowerCase() === "2"){
+			Creature();
+		}
+		else if (input.toLowerCase() === "3"){
+			StonePath();
+		}
+	}
+	waitForInput(processInput);
+}
+function BendInspect() {
+	clear();
+	print("\nYou find a thick jacket made of a strong material, feeling a chill in the air you feel inclined to put it on, it smells like old cologne and leaf decay, which it matches the color of.");
+	print("\nIt is very comfortable and soft on the inside, strangely you feel both protected and strengthened, and you look pretty cool.");
+	print("\n\tPress B to go back");
+
+	function processInput(input){
+		if (input.toLowerCase() === "b"){
+			StoneBend();
 		}
 	}
 	waitForInput(processInput);
