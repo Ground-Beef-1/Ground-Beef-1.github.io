9cc0d1122fa931b68347270acd1bc7790b8312c2
Ground-Beef-1 <mjbiluvdogs@gmail.com>
2026-03-09 14:22:41 -0400

    first ending

--- projects/game/game.js
+++ projects/game/game.js
@@ -8,35 +8,49 @@ let gameActive = true; //this variable is required.
 
 
 //Make one function for each location
-function locationA() {
+function Clearing() {
     clear();
-    print("\nYou are in location A!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
+    print("\nYou wake up in a strange clearing devoid of life with nothing but the clothes on your back and a dead phone in your pocket. The clearing seems to be blocked off by a dense wall of trees other than a few possible pathways. You feel uneasy, hungry, and quite lost given you have no idea where this place could be or how you got here.");
+    print("\nWhat will you do? Type the number associated with your choice" +
+        "\n\t1-   Give up, this clearing is your new home and the trees the harbingers of your fate" 
+	    + "\n\t2-   Inspect the far path with worn stone leading around a bend" + "\n\t3-    Inspect the beaten path to your left" + "\n\t4-    Inspect the clearing, there’s gotta be something, right?");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationb") {
-            locationB();
-        } else {
-            stayHere();
-            waitThenCall(locationA);
+        if (input.toLowerCase() === "1") {
+            Quitter();
+        } else if (input.toLowerCase() === "2"){
+            BeatenPath();
         }
+	  else if (input.toLowerCase() === "3"){
+		Unwitten();
+	  }
+	  else if (input.toLowerCase() === "4"){
+		Unwritten();
+	  }
     }
     waitForInput(processInput);
 }
 
-function locationB() {
+function Unwritten(){
+	clear();
+	print("I have not gotten so far as to add this arc yet, I'm just a guy");
+
+}
+
+function Quitter() {
     clear();
-    print("\nYou are in location B!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+    print("A voice echoes seemingly from the trees themselves -‘You’re really gonna give up, huh?" + 
+	    "\n <em>Weakling</em>" + "\n We thought you were better than the others, stronger, braver, but no you are just like all the rest. You deserve this fate. We have seen this happen enough times to know you are beyond help, but seemingly not enough times to stop our hope for something different’-" 
+ + 
+"\n\nYou feel the weight of what your life could have been if you kept on. It's like watching the life of another, it plays as you drift off to sleep, never to wake.");
+    print("\n\tPress C to continue");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationa") {
-            locationA();
+        if (input.toLowerCase() === "1") {
+            Clearing();
         } else {
             stayHere();
-            waitThenCall(locationB);
+            waitThenCall(Quitter);
         }
     }
     waitForInput(processInput);
@@ -46,10 +60,10 @@ function locationB() {
 //very start. For this simple example, any input will bring you
 //to locationA
 function start(){
-    print("Welcome to my game! Press any key to start");
+    print("Welcome, press any key to start");
 
     function processInput(input){
-            locationA();
+            Clearing();
     }
     waitForInput(processInput);
 }
